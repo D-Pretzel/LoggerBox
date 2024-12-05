d8e50916f3a93526aac2ffa756261b1ed371f68b
D-Pretzel <petzoldavid02@gmail.com>
2024-12-04 19:46:22 -0700

    Finalized ChainsawView & associated form

---
+++ LoggerBox/Views/NewFlightFormView.swift
@@ -0,0 +1,18 @@
+//
+//  NewFlightFormView.swift
+//  LoggerBox
+//
+//  Created by David Petzold on 12/4/24.
+//
+
+import SwiftUI
+
+struct NewFlightFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewFlightFormView()
+}
---
+++ LoggerBox/Views/RelightView.swift
@@ -0,0 +1,18 @@
+//
+//  RelightView.swift
+//  LoggerBox
+//
+//  Created by David Petzold on 12/4/24.
+//
+
+import SwiftUI
+
+struct RelightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RelightView()
+}
